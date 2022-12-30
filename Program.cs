6d1c8c8923dc5bbabc951b2470bc95fce2f9399b
ehabelhadry <121637941+ehabelhadry@users.noreply.github.com>
2022-12-30 02:59:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Enter the lower limit: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the upper limit: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The perfect numbers between {0} and {1} are:", n1, n2);
+
+            for (int i = n1; i <= n2; i++)
+            {
